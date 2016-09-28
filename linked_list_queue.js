c2d6b0e5c4e24208581a586fcc8e9b0726075f63
name": null, &   "name": null <pair+dev.wind@devbootcamp.com>
2016-09-28 09:49:07 +0700

    Template

---
+++ linked_list_queue.js
@@ -0,0 +1,8 @@
+//write your code here
+
+let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(list.getLength());
+console.log(list.getHead());
+console.log(list.getTail());
+list.push(11);
+list.pop();
