32e5f4c80b229377f5108005eceeb2b95b42ad18
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-02 21:48:46 +0700

    tugas linked list

--- linked_list_array.js
+++ linked_list_array.js
@@ -1,6 +1,28 @@
 //write your code here
+class LinkedList {
+    constructor(property) {
+        this.head = property[0]
+        this.tail = property[property.length - 1]
+        this.count = property.length
+    }
+
+    getLength() {
+        return `Length : ${this.count}`
+    }
+
+    getHead() {
+        return `Head : ${this.head}`
+    }
+
+    getTail() {
+        return `Tail : ${this.tail}`
+    }
+
+}
 
 let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(list.getLength());
 console.log(list.getHead());
 console.log(list.getTail());
+// console.log(list.getHead());
+// console.log(list.getTail());
--- linked_list_queue.js
+++ linked_list_queue.js
@@ -1,8 +1,44 @@
 //write your code here
+class LinkedList {
+    constructor(property) {
+        this.link = property
+        this.head = this.link[0]
+        this.tail = this.link[this.link.length - 1]
+        this.count = this.link.length
+    }
+
+    getLength() {
+        this.count = this.link.length
+        return `Length : ${this.count}`
+    }
+
+    getHead() {
+        return `Head : ${this.head}`
+    }
+
+    getTail() {
+        return `Tail : ${this.tail}`
+    }
+
+    push(value) {
+        this.link.push(value)
+        this.tail = this.link[this.link.length - 1]
+        console.log(`push message : Added ${value} to tail`);
+    }
+    pop() {
+        console.log(`pop message : Last data (${this.tail}) was removed`);
+        this.link.pop()
+        this.tail = this.link[this.link.length - 1]
+    }
+
+}
 
 let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(list.getLength());
 console.log(list.getHead());
 console.log(list.getTail());
 list.push(11);
+console.log(list.getTail());
+console.log(list.getLength());
 list.pop();
+console.log(list.getTail());
